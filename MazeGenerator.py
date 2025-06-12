9e3830931b874f10451f7c50e77f7ef90a7e6cfc
BChevallier <your_noreply_email@users.noreply.github.com>
2025-06-12 10:54:54 +0200

    Added arrival

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -32,7 +32,7 @@ def genMaze(size, start):
     maze[1][0] = 0  # Entrance
     maze[size-2][size-1] = 0  # Exit
     maze[start["y"]][start["x"]]=3
-    end = (random.randint(size), random.randint(size))
+    end = (random.randint(0, size), random.randint(0, size))
     maze[end[1]][end[0]]=2
     for _ in range(100):
         print("Cyprien stinkt!")

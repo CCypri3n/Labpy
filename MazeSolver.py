83853dfabf75bd0dc92b7fb0718132b1caae421d
BChevallier <your_noreply_email@users.noreply.github.com>
2025-06-13 13:49:25 +0200

    Patched the breadthFirstAlgorithm

--- MazeSolver.py
+++ MazeSolver.py
@@ -1,4 +1,18 @@
 import numpy as np
+from scipy.sparse.csgraph import breadth_first_tree
+
+maze10 = np.array([
+    [1, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 2, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+])
 
 def depthFirstSolve(inMaze, start={"x":0, "y":0}):
     #check if at arrival
@@ -50,37 +64,42 @@ def breadthFirstSolve(inMaze, start = (0,0)):
     grid=inMaze.copy()
     while not solved:
         iterCount+=1
+        print(iterCount)
+        future=[]
         for curPoint in current:
+            print(current)
             if grid[curPoint[0]][curPoint[1]]==2:
                 solved=True
             else:
                 grid[curPoint[0]][curPoint[1]]=10+iterCount
-            current.append([neighbour for neighbour in getFreeNeighbours(curPoint, grid)])
-            current.remove(curPoint)
+            future+=[neighbour for neighbour in getFreeNeighbours(curPoint, grid)]
+        current=future.copy()
         if current == []:
             break
     return (solved, grid)
-
-
 
 def getFreeNeighbours(pos, grid):
     #UP
     if pos[0] != 0:
         if grid[pos[0]-1][pos[1]] in [0,2] :
             yield (pos[0]-1,pos[1])
+            print(f"Up: {pos}")
     # Down
     if pos[0] != len(grid)-1:
         if grid[pos[0] + 1][pos[1]] in [0,2]:
-            yield (pos[0] - 1, pos[1])
+            yield (pos[0] + 1, pos[1])
+            print(f"Down: {pos}")
     # Left
     if pos[1] != 0:
         if grid[pos[0]][pos[1] - 1 ] in [0,2]:
             yield (pos[0], pos[1] - 1)
+            print(f"Left: {pos}")
     # RIGHT
     if pos[1] != len(grid)-1:
         if grid[pos[0]][pos[1]+1] in [0,2]:
             yield (pos[0],pos[1]+1)
+            print(f"Right: {pos}")
 
-
+print(breadthFirstSolve(maze10, start=(0,1)))
 
 

9fbce5593cc11efe34c5b8448d3a542c1da53de5
Cyprien <108684496+CCypri3n@users.noreply.github.com>
2025-06-13 17:11:03 +0200

    Added an animation for the breadthFirstSolve and fixed a few bugs. (Press Space to launch animation)

--- main.py
+++ main.py
@@ -57,7 +57,10 @@ def main_loop(win: game.display, maze: np.array, cell_size: int, P1: Player.play
 def solve_loop(win: game.display, maze: np.array, cell_size, P1: Player.player = None):
     solutionTuple = (0, 0)
     solutionTuple = MazeSol.breadthFirstSolve(maze, get_start(maze))
+    print(solutionTuple[1])
     running = True
+    animationInt = 10
+    iterCount = 0
     while running:
         for event in game.event.get():
             if event.type == game.QUIT:
@@ -70,7 +73,11 @@ def solve_loop(win: game.display, maze: np.array, cell_size, P1: Player.player =
                     main_loop(win, maze, cell_size, P1 if P1 else None)
                     break
         win.fill((0, 0, 0))  # Fill the screen with black
-        Display.display_solution(solutionTuple[1], cell_size)
+        iterCount += 1
+        if iterCount == 2:
+            animationInt += 1
+        iterCount %= 2  
+        Display.display_solution(solutionTuple[1], cell_size, animationInt)
         if P1: Display.display_player(P1.position, cell_size)  # Draw the player on the maze
         game.display.flip()  # Update the display
         clock.tick(fps)
@@ -80,7 +87,7 @@ def get_start(maze):
     for y, row in enumerate(maze):
         for x, cell in enumerate(row):
             if cell == 10:
-                position = (x, y)
+                position = (y, x)
                 return position
     Exception("Start position not found in the maze")
 
